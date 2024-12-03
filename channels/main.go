8d8582cbf197ea8339d16b9d1e58264caff60fc8
kairavb <kairavb8@gmail.com>
2024-12-03 19:53:37 +0530

    channels 2

--- channels/main.go
+++ channels/main.go
@@ -1,18 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const MaxpriceRice = 6
+const MaxpricePaneer = 4
 
 func main() {
-	var c = make(chan int)
-	go process(c)
-	for i := range c {
-		fmt.Println(i)
+	var riceChannel = make(chan string)
+	var paneerChannel = make(chan string)
+	var websites = []string{"walmart.com", "costco.com", "wholefoods.com"}
+
+	for i := range websites {
+		go checkRicePrices(websites[i], riceChannel)
+		go checkPaneerPrices(websites[i], paneerChannel)
+	}
+
+	sendMessage(riceChannel, paneerChannel)
+}
+
+func checkRicePrices(website string, c chan string) {
+	for {
+		time.Sleep(time.Second * 1)
+		var price = rand.Float32() * 20
+		if price < MaxpriceRice {
+			c <- website
+			break
+		}
 	}
 }
 
-func process(c chan int) {
-	for i := 0; i < 5; i++ {
-		c <- i
+func checkPaneerPrices(website string, c chan string) {
+	for {
+		time.Sleep(time.Second * 1)
+		var price = rand.Float32() * 20
+		if price < MaxpricePaneer {
+			c <- website
+			break
+		}
 	}
-	close(c)
+}
+
+func sendMessage(r chan string, p chan string) {
+	select {
+	case website := <-r:
+		fmt.Println("Found a deal on rice at: ", website)
+	case website := <-p:
+		fmt.Println("Found a deal on panner at: ", website)
+	}
 }
